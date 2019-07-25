5128ad56717dcf792f3736358aae432844907da3
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-07-25 09:04:01 +0530

    Add initial files

---
+++ js/sketch.js
@@ -0,0 +1,14 @@
+// '': empty; 'o': zero; 'x': cross
+let board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+];
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
