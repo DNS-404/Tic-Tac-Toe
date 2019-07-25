13c595b57ffaf4cf4c246e499e8c45ac8d0191f6
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-07-25 12:00:46 +0530

    Complete game

--- js/sketch.js
+++ js/sketch.js
@@ -7,16 +7,17 @@ let grid = [
 
 let players = ['o', 'x'];
 let currentPlayer;
+let w, h;
 
 function setup() {
   createCanvas(400, 400);
-  currentPlayer = random(players);
+  currentPlayer = players[0];
+  w = width/3;
+  h = height/3;
 }
 
 function draw() {
   background(220);
-  let w = width/3;
-  let h = height/3;
 
   // draw lines
   strokeWeight(1);
@@ -32,9 +33,7 @@ function draw() {
     for(let col=0; col<3; col++){
       let x = w*col + w/2;
       let y = h*row + h/2;
-      // console.log(floor(x) + ", " + floor(y));
       let cellContent = grid[row][col];
-      // console.log(cellContent);
       if(cellContent == players[0]){
         noFill();
         ellipse(x,y,w/2);
@@ -45,6 +44,54 @@ function draw() {
       }
     }
   }
+
+  let winner = getWinner();
+  if(winner != null){
+    console.log(winner + " won!");
+    noLoop();
+  }
+}
+
+function getWinner() {
+  let winner = null;
+  // check rows
+  for(let row=0; row<3; row++){
+    if(grid[row][0] == grid[row][1] && grid[row][1] == grid[row][2] && grid[row][0] != ''){
+      let x = w/2;
+      let y = h*row + h/2;
+      strokeWeight(4);
+      stroke(255, 0, 0);
+      line(x-w/4, y, x+2*w+(w/4), y);
+      winner = grid[row][0];
+      break;
+    }
+  }
+  // check cols
+  for(let col=0; col<3; col++){
+    if(grid[0][col] == grid[1][col] && grid[1][col] == grid[2][col] && grid[0][col] != ''){
+      let x = w*col + w/2;
+      let y = h/2;
+      strokeWeight(4);
+      stroke(255, 0, 0);
+      line(x, y-h/4, x, y+2*h+(h/4));
+      winner = grid[0][col];
+      break;
+    }
+  }
+  // check diagnols
+  if(grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[0][0] != ''){
+    strokeWeight(4);
+    stroke(255, 0, 0);
+    line(w/4, h/4, width - (w/4), height - (h/4));
+    winner = grid[0][0];
+  }
+  if(grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0] && grid[0][2] != ''){
+    strokeWeight(4);
+    stroke(255, 0, 0);
+    line(width - (w/4), h/4, w/4, height - (h/4));
+    winner = grid[0][2];
+  }
+  return winner;
 }
 
 function mousePressed() {
@@ -54,5 +101,15 @@ function mousePressed() {
   if(grid[yVal][xVal] == ''){
     grid[yVal][xVal] = currentPlayer;
     currentPlayer = (currentPlayer == players[0]) ? players[1] : players[0];
+  }
+}
+
+function keyPressed() {
+  if(keyCode == 32){ // space to restart game
+    for(let row=0; row<3; row++){
+      for(let col=0; col<3; col++){
+        grid[row][col] = '';
+      }
+    }
   }
 }
