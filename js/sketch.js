04c69f956c82168567314d8974089d3ac64596a4
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-07-25 10:59:15 +0530

    Basic game setup

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,50 @@
 // '': empty; 'o': zero; 'x': cross
-let board = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
+let grid = [
+  ['', '', 'x'],
+  ['o', 'x', ''],
+  ['', '', 'x'],
 ];
 
+let players = ['o', 'x'];
+let currentPlayer;
+
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
+  if(random(1) < 0.5){
+    currentPlayer = players[0];
+  } else {
+    currentPlayer = players[1];
+  }
 }
 
 function draw() {
-  // put drawing code here
+  background(220);
+  let w = width/3;
+  let h = height/3;
+
+  // draw lines
+  strokeWeight(1);
+  for(let i=1; i<3; i++){
+    line(w*i, 0, w*i, height);
+    line(0, h*i, width, h*i);
+  }
+
+  // current grid situation
+  textSize(32);
+  strokeWeight(2);
+  for(let i=0; i<3; i++){
+    for(let j=0; j<3; j++){
+      let x = w*i + w/2;
+      let y = h*j + h/2;
+      let cellContent = grid[i][j];
+      if(cellContent == players[0]){
+        noFill();
+        ellipse(x,y,w/2);
+      }else if(cellContent == players[1]){
+        let off = w/4;
+        line(x-off, y-off, x+off, y+off);
+        line(x+off, y-off, x-off, y+off);
+      }
+    }
+  }
 }
